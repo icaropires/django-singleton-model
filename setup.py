d7464c75c2598f357e23f28844773a52b529261a
Ícaro Pires <icaropsa@gmail.com>
2018-05-25 22:39:25 -0300

    Add some more metadata files

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import os
 from setuptools import setup
 
 install_requires = [
     'django'
 ]
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name='django_singleton_model',
     version='0.1',
     description='Simple package for impleting models using singleton design pattern',
-    url='https://github.com/icaropires/django-singletonmodel',
+    url='https://github.com/icaropires/django-singleton-model',
     author='Ícaro Pires',
     author_email='icaropsa@gmail.com',
     license='MIT',
@@ -16,4 +20,14 @@ setup(
     include_package_data=True,
     packages=['singleton_model'],
     install_requires=install_requires,
+    long_description=read('README.md'),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
